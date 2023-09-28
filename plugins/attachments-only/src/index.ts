a4339d47e7d857c1613fa499273d965af6bd076b
Camlan Wilson-Snowden <camlanmail@gmail.com>
2023-09-29 01:08:48 +0930

    feat: attachments-only plugin

--- plugins/attachments-only/src/index.ts
+++ plugins/attachments-only/src/index.ts
@@ -1,9 +1,12 @@
 import { logger } from "@vendetta";
+import vendetta from "@vendetta"
 import Settings from "./Settings";
 
 export default {
     onLoad: () => {
         logger.log("Hello world!");
+        logger.log(window)
+        logger.log(vendetta)
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
